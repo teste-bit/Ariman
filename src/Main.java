a6887725702b70a58234181077b298f97acbc4c2
teste-bit <crimedomathias@gmail.com>
2020-07-05 22:37:12 -0300

    projeto de teste

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Locale;
+
+public class Main{
+	public static void main(String[] args) {
+		
+		
+		int x=20;
+		
+		qqmerda bosta = new qqmerda();
+		
+		bosta.a=x;
+		
+		int y=bosta.a;
+		System.out.println(y);	
+		
+	}
+	
+}
+
+
+
